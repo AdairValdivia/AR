4b2be186a407ccbaa1aa42c495961340da615f30
Unknown <lilith.de.cain@gmail.com>
2018-08-29 01:23:06 -0500

    imagen post

--- mainAR.py
+++ mainAR.py
@@ -1,5 +1,10 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np 
+import cv2
+import time
 app = Flask(__name__)
 @app.route('/cargarImagen')
 def cargarImagen():
@@ -7,6 +12,15 @@ def cargarImagen():
     
 @app.route('/AR', methods=['POST'])
 def AR():
+    a=request.files['image']
+    nparr = np.fromstring(a, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    #print(type(a))
+    #img=mpimg.imread(a)
+    #imgplot = plt.imshow(img)
+    #plt.show()
+    #time.sleep(5)
     return jsonify({"mensaje":"hola mundo"})
 
 if __name__ == "__main__":
